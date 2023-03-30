8e4d4fa819e04bb58b63e764db1a522d848247be
xavieracaling <xavier.acaling@narrasoft.com>
2023-03-31 05:58:02 +0800

    init set game values

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManager : MonoBehaviour
+{
+    public static BarManager Instance;
+    private void Awake() {
+        Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MahjongManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MahjongManager : MonoBehaviour
+{
+    public static MahjongManager Instance;
+    public Transform MahjongContainer;
+    private void Awake() {
+        Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
